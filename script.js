b62646fc961881bcfe3582c4c2d5ec2d6ce29903
luz Villar <luzvf18@gmail.com>
2021-05-03 19:41:14 -0500

    Comentarios, variables, cadenas, constantes

---
+++ script.js
@@ -0,0 +1,35 @@
+// alert('Hola numdo)
+console.log('Hola mundo');
+
+
+// comentario de linea 
+/* 
+comentario de 
+varias lineas 
+*/
+
+console.log('Linea uno');
+console.log('Linea dos');
+
+// Variables
+
+let a = 5;
+console.log(a);
+
+// Mensajes
+let nombre = Luz;
+console.log('Hola mundo',nombre);
+console.log("Hola mundo"+nombre);
+console.log(`Hola mundo ${nombre}`);
+
+let nombreCompleto = 'Luz Villar';
+let nombre_completo = 'Luz villar';
+
+let $ = 5;
+
+// Constantes 
+
+const PI = 3.1416;
+const COLOR = '';
+
+// const codigo = getId();
